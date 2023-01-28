15773e6b7c68eaaa7bfa6d15ec1ef9b827c47624
Kamil Nafalski <kamilnafalski@gmail.com>
2023-01-28 12:06:51 +0100

    Add getAttribute and targetArticle

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
     console.log('links:', links);
   });*/
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
+    
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -17,16 +19,19 @@ const titleClickHandler = function (event) {
     console.log('clickedElement (with plus): ' + clickedElement);
     
     /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll(' .post.active');
+    const activeArticles = document.querySelectorAll('.post.active');
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
