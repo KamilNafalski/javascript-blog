c10faddb72a7c2efbb875aa96cc4ae16b876fea4
Kamil Nafalski <kamilnafalski@gmail.com>
2023-02-06 21:27:39 +0100

    Add start loop

--- js/script.js
+++ js/script.js
@@ -36,7 +36,8 @@ const titleClickHandler = function (event) {
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
 
@@ -76,3 +77,35 @@ function generateTitleLinks() {
 }
 generateTitleLinks();
 
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const titleList = article.querySelector(optArticleTagsSelector);
+    /* make html variable with empty string */
+    let HTML = ' ';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+  
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+        console.log(tag);  
+      /* generate HTML of the link */
+  
+      /* add generated code to html variable */
+    }
+  }
+    /* END LOOP: for each tag */
+  
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+  
+generateTags();
